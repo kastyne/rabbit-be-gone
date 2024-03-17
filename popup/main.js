3ed4d0c20f575e7cef3189c63ccdceec7d3d72c2
haaruun <haaruunibrow@gmail.com>
2024-03-17 08:22:13 +0300

    Add Alpinejs & ui shell

--- popup/main.js
+++ popup/main.js
@@ -1,12 +1,45 @@
-const historyList = document.getElementById("historyList")
-const heading = document.getElementById('heading')
+// alpine has to be initialized in this odd way to follow the CSP rules for extentions
+document.addEventListener('alpine:init', () => { 
+    Alpine.data('model', () => ({
+        currentPage: "home",
 
+        score: 10,
+        scoreTxt: `Score: ${this.score}`,
+        history: [],
+        changePage(event) {
+            this.currentPage = event.target.id
+
+            this.homePage = this.currentPage == 'home' ? 'currPage' : 'hidden' 
+            this.listPage = this.currentPage == 'list' ? 'currPage' : 'hidden'
+            this.settingsPage = this.currentPage == 'settings' ? 'currPage' : 'hidden'
+            
+        },
+
+        // this is the only way i can get reactivity to work in CSP mode
+        init() {
+            this.homePage = "currPage"
+            this.listPage = "hidden"
+            this.settingsPage = "hidden"
+        }
+    }))
+})
+
+/*
 chrome.storage.local.get().then(localStorage => {
     heading.innerText = `Score: ${localStorage.score}`
 
     localStorage.historyItems.forEach(item => {
-        let itemElement = document.createElement('li')
+        let itemElement = document.createElement('lwi')
         itemElement.innerText = item.title + " --" + item.score
         historyList.appendChild(itemElement)  
+
+    settingsForm.addEventListener('submit', () => {
+        FormData(settingsForm).entries().forEach((entry) => {
+            key, value = entry
+            chrome.storage.sync.set([{
+                key, value
+            }])
+        })
     })
 })
+*/
